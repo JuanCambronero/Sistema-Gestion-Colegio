bc6be742f8361cb9c5c019f21e123456898c6316
Raul-data <rauljusto7@outlook.com>
2026-01-26 16:50:39 +0100

    Remove commented code and clean up main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from Personas import Persona
 
 print("=== PRUEBA PERSONA ===")
@@ -8,9 +7,5 @@ p.idPersona(1)
 p.nombre("Juan")
 p.email("Juan@gmail.com")
 p.mostrarInfo()
-=======
-#Hola soy sebastian
-print("Hola, soy Sebastian")
-print("Hola soy Guillermo")
-print("Segungo commit")
->>>>>>> 228d65af1559633ac8d9d43ae5be762a5df26a55
+
+
