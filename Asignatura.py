0c1227751a4764d5aa52f222cdd4943c57c51e8b
guillermo0602 <gui_w_17@hotmail.com>
2026-01-27 19:09:46 +0100

    Clase Asignatura

--- Asignatura.py
+++ Asignatura.py
@@ -1,1 +1,58 @@
 #Asignatura
+from Profesor import Profesor
+
+class Asignatura:
+    #Constructor de Asignatura
+    def __init__(self, id, nombre, profesor:Profesor, alumno: None):
+        self.id= id
+        self.nombre=nombre
+        self.profesor=profesor
+        self.alumno=alumno if alumno is not None else []
+
+    #getter
+    @property
+    def id(self):
+        return self.id
+    
+    @property
+    def nombre(self):
+        return self.nombre
+    
+    @property
+    def profesor(self):
+        return self.profesor
+    
+    @property
+    def alumno(self):
+        return self.alumno
+    
+    #setter
+    @id.setter
+    def id(self,nuevo_id:int):
+        self.id=nuevo_id
+
+    @nombre.setter
+    def nombre(self, nuevoNombre:str):
+        self.nombre=nuevoNombre
+
+    @profesor.setter
+    def profesor(self, nuevoProfesor:str):
+        self.profesor=nuevoProfesor
+
+    @alumno.setter
+    def alumno(self, nuevoAlumno:list):
+        self.alumno=nuevoAlumno
+    
+    #Metodo para asignar Profesor
+    def asignarProfesor(self,profesor:Profesor): 
+        self.profesor=profesor
+
+    #Metodo para inscribir alumno
+    def inscribirAlumno(self, alumnos):
+        if alumnos not in self.alumno:
+            self.alumno.append(alumnos)
+
+    #Metodo para listar alumnos
+    def listar(self):
+        return self.alumno
+
